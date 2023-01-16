113734471eb648ad3ca570fd57302ac835a02afa
adahnke1 <adahnke1@gmail.com>
2023-01-16 13:07:15 -0600

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -1,14 +1,17 @@
 import pybullet as p
 import time
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate(robotId)
 for i in range(1000):
     p.stepSimulation()
     time.sleep(1/600)
     print(i)
+backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 p.disconnect()
