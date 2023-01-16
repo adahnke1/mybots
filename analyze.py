e73115c1603a3d1d3f34424a27485b88cfb9854c
adahnke1 <adahnke1@gmail.com>
2023-01-16 16:20:19 -0600

    slay

--- analyze.py
+++ analyze.py
@@ -1,6 +1,10 @@
 import numpy
 import matplotlib.pyplot
 backLegSensorValues = numpy.load('/Users/adahnke1/Documents/GitHub/mybots/data/backLegvalues.npy')
+frontLegSensorValues = numpy.load('/Users/adahnke1/Documents/GitHub/mybots/data/frontLegvalues.npy')
 print(backLegSensorValues)
-matplotlib.pyplot.plot(backLegSensorValues)
+print(frontLegSensorValues)
+matplotlib.pyplot.plot(backLegSensorValues, label='Back Leg',linewidth=2)
+matplotlib.pyplot.plot(frontLegSensorValues, label='Front Leg',linewidth=1)
+matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
