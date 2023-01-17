e47d2f8af7afedaaebeca1a5d18d6d894833a8f8
adahnke1 <adahnke1@gmail.com>
2023-01-16 21:07:16 -0600

    ggg

--- simulate.py
+++ simulate.py
@@ -3,6 +3,7 @@ import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy
+import random
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
@@ -17,8 +18,8 @@ for i in range(500):
     time.sleep(1/600)
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId,jointName = b"Torso_BackLeg",controlMode = p.POSITION_CONTROL,targetPosition = -0.5,maxForce = 500)
-    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId,jointName = b"Torso_FrontLeg",controlMode = p.POSITION_CONTROL,targetPosition = 0.5,maxForce = 500)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId,jointName = b"Torso_BackLeg",controlMode = p.POSITION_CONTROL,targetPosition = random.random(),maxForce = 500)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId,jointName = b"Torso_FrontLeg",controlMode = p.POSITION_CONTROL,targetPosition = random.random(),maxForce = 500)
 print(backLegSensorValues)
 print(frontLegSensorValues)
 numpy.save('/Users/adahnke1/Documents/GitHub/mybots/data/backLegvalues', backLegSensorValues, allow_pickle = True, fix_imports = True)
